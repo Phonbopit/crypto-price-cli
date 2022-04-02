e727159923d80f93dbc1cc7dc8c5bd1e8a61dded
Chai Phonbopit <phonbopit@gmail.com>
2022-04-02 21:31:46 +0700

    finish simple fetch data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use serde::Deserialize;
+use reqwest as request;
+use reqwest::Error;
 
 #[derive(Parser)]
 #[clap(name="CryptoPrice")]
@@ -7,17 +10,44 @@ use clap::Parser;
 #[clap(about = "CLI to get crypto price from ftx spot market.")]
 struct Args {
     // Name of the market.
-    #[clap(short, long, default_value="market")]
-    name: String
+    #[clap(short, long, default_value="BTC/USD")]
+    market: String
 }
 
-#[allow(unused)]
-fn fetch() {
-    // TODO: // fetch data from api.
+#[derive(Deserialize, Debug)]
+struct Market {
+    name: String,
+    price: f32,
+
+    #[serde(rename = "volumeUsd24h")]
+    volume_usd24h: f32,
+
+    #[serde(rename = "type")]
+    market_type: String
 }
 
+#[derive(Deserialize, Debug)]
+struct APIResponse {
+    success: bool,
+    result: Market
+}
+
+#[tokio::main]
+async fn fetch(market: &str) -> Result<APIResponse, Error> {
+    let url = format!("https://ftx.com/api/markets/{market}", market=market);
+
+    println!("{}", url);
+
+    let response: APIResponse = request::get(url)
+        .await?.json().await?;
+
+    Ok(response)
+}
+
+// #[tokio::main]
 fn main() {
     let args = Args::parse();
 
-    println!("Hello, world! {:?}", args.name);
+    let data = fetch(&args.market);
+    println!("{:#?}", data);
 }
