b6be19654b49a0ff025a404f800e81b361f12ec5
Chai Phonbopit <phonbopit@gmail.com>
2022-04-02 20:19:35 +0700

    try to use clap derive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(name="CryptoPrice")]
+#[clap(author="Chai Phonbopit")]
+#[clap(version="1.0.0")]
+#[clap(about = "CLI to get crypto price from ftx spot market.")]
+struct Args {
+    // Name of the market.
+    #[clap(short, long, default_value="market")]
+    name: String
+}
+
+#[allow(unused)]
+fn fetch() {
+    // TODO: // fetch data from api.
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello, world! {:?}", args.name);
 }
